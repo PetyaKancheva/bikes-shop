362c1b3da1b6d719557f8f727c42b764709bfe71
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-08-23 18:40:51 +0300

    Update CurrencyServiceImplTest.java

--- src/test/java/bg/softuni/bikes_shop/service/impl/CurrencyServiceImplTest.java
+++ src/test/java/bg/softuni/bikes_shop/service/impl/CurrencyServiceImplTest.java
@@ -1,72 +1,87 @@
 package bg.softuni.bikes_shop.service.impl;
 
+import bg.softuni.bikes_shop.model.dto.CurrencyExchangeDTO;
+import bg.softuni.bikes_shop.model.entity.CurrencyEntity;
 import bg.softuni.bikes_shop.repository.CurrencyRepository;
 
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.web.servlet.LocaleResolver;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CurrencyServiceImplTest {
     private CurrencyServiceImpl serviceToTest;
+    private MockHttpServletRequest mockRequest;
+    private MockHttpServletResponse mockResponse;
+
     @Mock
     private CurrencyRepository mockCurrencyRepository;
     @Mock
     private LocaleResolver localeResolver;
 
+
     @BeforeEach
     void setUp() {
         serviceToTest = new CurrencyServiceImpl(mockCurrencyRepository, localeResolver);
-
+        mockRequest = new MockHttpServletRequest();
+        mockResponse = new MockHttpServletResponse();
     }
 
     @Test
     void testUpdateLocalBGN() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        MockHttpServletResponse response = new MockHttpServletResponse();
         String selectedCurrency = "BGN";
 
-        serviceToTest.updateLocale(request,response,selectedCurrency);
+        serviceToTest.updateLocale(mockRequest, mockResponse, selectedCurrency);
 
-        Assertions.assertEquals("bg",response.getLocale().getLanguage());
+        Assertions.assertEquals("bg", mockResponse.getLocale().getLanguage());
     }
+
     @Test
-    void testUpdateCookieNoCookie(){
-
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    void testUpdateCookieNoCookie() {
         String selectedCurrency = "BGN";
 
-        serviceToTest.updateCookie(request,response,selectedCurrency);
+        serviceToTest.updateCookie(mockRequest, mockResponse, selectedCurrency);
 
-        Assertions.assertEquals(selectedCurrency,response.getCookie("currency").getValue());
+        assertEquals(selectedCurrency, mockResponse.getCookie("currency").getValue());
     }
+
     @Test
-    void testUpdateCookieWithCookieEURotBGN(){
+    void testUpdateCookieWithCookieEURotBGN() {
         String selectedCurrency = "BGN";
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        Cookie cookie= new Cookie("currency", "EUR");
+        Cookie cookie = new Cookie("currency", "EUR");
 
-        MockHttpServletResponse response = new MockHttpServletResponse();
+        mockRequest.setCookies(cookie);
 
-       request.setCookies(cookie);
+        serviceToTest.updateCookie(mockRequest, mockResponse, selectedCurrency);
 
-        serviceToTest.updateCookie(request,response,selectedCurrency);
-
-        Assertions.assertEquals(selectedCurrency,response.getCookie("currency").getValue());
+        assertEquals(selectedCurrency, mockResponse.getCookie("currency").getValue());
     }
+
+    @Test
+    void testGetCurrencyDTO() {
+
+        CurrencyEntity currencyBGN = new CurrencyEntity().setName("BGN").setRate(BigDecimal.valueOf(1.96));
+
+        when(mockCurrencyRepository.findByName(currencyBGN.getName())).thenReturn(currencyBGN);
+        when(localeResolver.resolveLocale(mockRequest)).thenReturn(Locale.ENGLISH);
+
+        CurrencyExchangeDTO result = serviceToTest.getCurrencyDTO(mockRequest, mockResponse, currencyBGN.getName());
+
+        Assertions.assertEquals(currencyBGN.getRate().doubleValue(), result.rate());
+
+    }
+
 }
