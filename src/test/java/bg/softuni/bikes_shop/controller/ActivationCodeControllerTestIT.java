5238365bccac63217e12dc3408fccc96fceb219b
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-08-24 11:55:42 +0300

    Create ActicationCodeControllerTest

--- src/test/java/bg/softuni/bikes_shop/controller/ActivationCodeControllerTestIT.java
+++ src/test/java/bg/softuni/bikes_shop/controller/ActivationCodeControllerTestIT.java
@@ -1,6 +1,12 @@
 package bg.softuni.bikes_shop.controller;
 
+import bg.softuni.bikes_shop.model.entity.UserActivationCodeEntity;
 import bg.softuni.bikes_shop.repository.ActivationCodeRepository;
+import bg.softuni.bikes_shop.util.TestDataUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,15 +24,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ActivationCodeControllerTestIT {
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    TestDataUtil testDataUtil;
+
+
+    @BeforeEach
+    void setUp(){
+        testDataUtil.cleanUp();
+    }
+    @AfterEach
+    void tearDown(){
+        testDataUtil.cleanUp();
+    }
 
 
     @Test
-    void  testActivateUser() throws Exception {
+    void  testActivateUserFail() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/user/activate/")
                         .param("activation_code","jmCY4WoeBarWrdb")
                 .with(csrf()))
-                .andExpect(status().isOk());
+                .andExpect(status().is3xxRedirection())
+                .andExpect(flash().attribute("message","Not able to activate code."));
+    }
+    @Test
+    void  testActivateUserSuccess() throws Exception {
 
+        UserActivationCodeEntity testEntity=testDataUtil.createActivationCode();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/activate/")
+                        .param("activation_code",testEntity.getActivationCode())
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(flash().attribute("message","Congratulations! Account is now activated! Please log in. "));
     }
 
 
--- src/test/java/bg/softuni/bikes_shop/util/TestDataUtil.java
+++ src/test/java/bg/softuni/bikes_shop/util/TestDataUtil.java
@@ -1,5 +1,6 @@
 package bg.softuni.bikes_shop.util;
 
+import bg.softuni.bikes_shop.model.UserRoleEnum;
 import bg.softuni.bikes_shop.model.dto.AdminUpdateDTO;
 import bg.softuni.bikes_shop.model.dto.ProductAddDTO;
 import bg.softuni.bikes_shop.model.entity.*;
@@ -7,7 +8,11 @@ import bg.softuni.bikes_shop.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 import java.math.BigDecimal;
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.util.List;
 
 @Component
 public class TestDataUtil {
@@ -22,7 +27,10 @@ public class TestDataUtil {
     private ItemRepository itemRepository;
     @Autowired
     private ActivationCodeRepository activationCodeRepository;
-
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserRoleRepository userRoleRepository;
 
 
     public void createExchangeRate(String currency, BigDecimal rate) {
@@ -38,8 +46,30 @@ public class TestDataUtil {
                         1000d, "testCategory", "urlTest");
 
     }
+    private UserEntity createUser( ) {
 
+        var roleEntities = userRoleRepository.findAllByNameIn(List.of(UserRoleEnum.USER));
+        UserEntity newUser = new UserEntity()
+                .setEnabled(false)
+                .setFirstName("First name test")
+                .setLastName("Last name test")
+                .setEmail("test@mail.com")
+                .setAddress("Address Test")
+                .setCountry("Country Test")
+                .setRoles(roleEntities)
+                .setPassword("test1234");
 
+        return userRepository.save(newUser);
+    }
+
+    public UserActivationCodeEntity createActivationCode(){
+        UserActivationCodeEntity newActivationCode= new UserActivationCodeEntity()
+                .setActivationCode("jmCY4WoeBarWrdb")
+                .setCreated( Instant.now())
+                .setUser(createUser());
+
+        return activationCodeRepository.save( newActivationCode);
+    }
     public ProductEntity createTestProduct() {
         ProductEntity newProduct = new ProductEntity()
                 .setName("test product")
@@ -58,12 +88,14 @@ public class TestDataUtil {
     }
 
     //
+
     public void cleanUp() {
         currencyRepository.deleteAll();
         productRepository.deleteAll();
         orderRepository.deleteAll();
         itemRepository.deleteAll();
         activationCodeRepository.deleteAll();
+        userRepository.deleteAll();
 
     }
 }
