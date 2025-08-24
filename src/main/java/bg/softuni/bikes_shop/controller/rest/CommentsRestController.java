3fcfd304c121a513db54f336c20ce9c5ceac5663
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-08-24 16:23:20 +0300

    Update restcontroller

--- src/main/java/bg/softuni/bikes_shop/controller/rest/CommentsRestController.java
+++ src/main/java/bg/softuni/bikes_shop/controller/rest/CommentsRestController.java
@@ -1,18 +1,10 @@
 package bg.softuni.bikes_shop.controller.rest;
 
-import bg.softuni.bikes_shop.exceptions.CustomObjectNotFoundException;
 import bg.softuni.bikes_shop.model.dto.CommentDTO;
 import bg.softuni.bikes_shop.service.CommentService;
-import jakarta.validation.Valid;
-import org.hibernate.query.QueryParameter;
-import org.springframework.data.annotation.QueryAnnotation;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/comments")
@@ -26,23 +18,20 @@ public class CommentsRestController {
     }
 
     @GetMapping("/all")
-    public Object getAll(){
-        return   restTemplate.getForEntity(commentService.getURLForAllComments(),Object[].class);
+    public Object getAll() {
+        return restTemplate.getForEntity(commentService.getURLForAllComments(), Object[].class);
     }
+
     @GetMapping("/comment")
-    public ResponseEntity<CommentDTO> getSingleComment(@RequestParam String id){
-        return  restTemplate.getForEntity(commentService.getURLForOneComment(Long.parseLong(id)), CommentDTO.class);
+    public ResponseEntity<CommentDTO> getSingleComment(@RequestParam String id) {
+        return restTemplate.getForEntity(commentService.getURLForOneComment(Long.parseLong(id)), CommentDTO.class);
     }
 
     @GetMapping("/delete")
-    public String deleteComment(@RequestParam String id){
+    public String deleteComment(@RequestParam String id) {
         String url = commentService.getURLForCommentDeletion(Long.parseLong(id));
-        System.out.println(url);
-          restTemplate.delete(url);
-          return "comment deleted";
+        restTemplate.delete(url);
+        return "comment deleted";
     }
 
-
-
-}
-
+ }
