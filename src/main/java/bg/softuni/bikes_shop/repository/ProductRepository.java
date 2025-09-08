64d68eef921f1105745cb7c76021df6c94005a7a
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-09-08 15:01:10 +0300

    Update ProductRepository.java

--- src/main/java/bg/softuni/bikes_shop/repository/ProductRepository.java
+++ src/main/java/bg/softuni/bikes_shop/repository/ProductRepository.java
@@ -33,18 +33,5 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     Optional<ProductEntity> findByCompositeName(String compositeName);
 
-
-//    @Query(value="SELECT *  FROM products WHERE MATCH (category, name, description) AGAINST( ? ) AND composite_name IS NOT NULL ",
-//            nativeQuery = true)
-//
-
-//    @Query(value="SELECT *  FROM products WHERE name = ? AND composite_name IS NOT NULL ",
-//            nativeQuery = true)
-    @Query(value="SELECT *  FROM products WHERE MATCH (category, name, description) AGAINST( ? ) AND composite_name IS NOT NULL ",
-            nativeQuery = true)
-    Page<ProductEntity> findAllByKeyword(String keyword, Pageable pageable);
-//
-//    @Query(value="SELECT *  FROM products WHERE MATCH (category, name, description) AGAINST( ? ) AND composite_name IS NOT NULL ",
-//            nativeQuery = true)
     Page<ProductEntity> searchAllByDescriptionContainingOrCategoryOrNameContaining(String description,String category,String name,Pageable pageable);
 }
