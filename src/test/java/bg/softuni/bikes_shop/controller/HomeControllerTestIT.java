817b3eebd028a455c44fd10005b7e7a8bd05009d
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-09-08 14:45:53 +0300

    Create HomeControllerTestIT.java

---
+++ src/test/java/bg/softuni/bikes_shop/controller/HomeControllerTestIT.java
@@ -0,0 +1,102 @@
+package bg.softuni.bikes_shop.controller;
+
+import bg.softuni.bikes_shop.model.CustomUserDetails;
+import bg.softuni.bikes_shop.model.entity.ProductEntity;
+import bg.softuni.bikes_shop.repository.ProductRepository;
+import bg.softuni.bikes_shop.util.TestDataUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.*;
+import java.util.Optional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTestIT {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private TestDataUtil testDataUtil;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        testDataUtil.cleanUp();
+    }
+
+    @AfterEach
+    void tearDown() {
+        testDataUtil.cleanUp();
+    }
+
+    @Test
+    void getIndexSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/")
+
+
+                        .with(csrf()))
+                .andExpectAll(status().isOk(), view().name("index")
+                        , model().attributeExists("products")
+                        , model().attributeExists("currDTO")
+                        , model().attributeExists("listCurrencies")
+                        , model().attributeExists("categories"));
+
+    }
+
+    @Test
+    void getAboutSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/about"))
+                .andExpectAll(status().isOk(), view().name("/static/about"));
+    }
+
+    @Test
+    void getServicesSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/services"))
+                .andExpectAll(status().isOk(), view().name("/static/services"));
+    }
+
+    @Test
+    void getContactsSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/contacts"))
+                .andExpectAll(status().isOk(), view().name("/static/contacts"));
+    }
+
+    @Test
+    void getError500Success() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/error/500"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void getSearchResultSuccess() throws Exception {
+
+        ProductEntity testProduct = testDataUtil.createTestProduct();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/search-result")
+                        .param("productToSearch", testProduct.getName())
+                        .with(csrf()))
+                .andExpect(status().isOk());
+
+    }
+
+}
